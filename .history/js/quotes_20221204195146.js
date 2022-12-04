35888ee773e171a64fae59aa9bf484c7e93cf645
Yoon <yoon.shin.255.255@gmail.com>
2022-12-04 20:07:03 +1300

    Random Quotes Feature
    Displaying random quotes on refresh.

---
+++ .history/js/quotes_20221204195146.js
@@ -0,0 +1,26 @@
+const quotes = [
+	{
+		quote: "When you have a dream, you've got to grab it and never let go.",
+		author: "Carol Burnett",
+	},
+	{
+		quote: "Nothing is impossible. The word itself says 'I'm possible!'",
+		author: "Audrey Hepburn",
+	},
+	{
+		quote: "There is nothing impossible to they who will try.",
+		author: "Alexander the Great",
+	},
+	{
+		quote: "The bad news is time flies. The good news is you're the pilot.",
+		author: "Michael Altshuler",
+	},
+	{
+		quote:
+			"Success is not final, failure is not fatal: it is the courage to continue that counts.",
+		author: "Winston Churchill",
+	},
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const auhtor = document.querySelector("#quote span:second-child");
---
+++ .history/js/quotes_20221204195827.js
@@ -0,0 +1,31 @@
+const quotes = [
+	{
+		quote: "When you have a dream, you've got to grab it and never let go.",
+		author: "Carol Burnett",
+	},
+	{
+		quote: "Nothing is impossible. The word itself says 'I'm possible!'",
+		author: "Audrey Hepburn",
+	},
+	{
+		quote: "There is nothing impossible to they who will try.",
+		author: "Alexander the Great",
+	},
+	{
+		quote: "The bad news is time flies. The good news is you're the pilot.",
+		author: "Michael Altshuler",
+	},
+	{
+		quote:
+			"Success is not final, failure is not fatal: it is the courage to continue that counts.",
+		author: "Winston Churchill",
+	},
+];
+
+const quote = document.querySelector("#quote");
+const auhtor = document.querySelector("#quote");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
